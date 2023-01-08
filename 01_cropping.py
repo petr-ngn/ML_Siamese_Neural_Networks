6439bfc0d0808645dabafa775a61249f9a412b3d
Petr Nguyen <110040418+petr-ngn@users.noreply.github.com>
2023-01-08 07:13:55 +0100

    Cropping, resizing and saving pictures

--- 01_cropping.py
+++ 01_cropping.py
@@ -6,55 +6,59 @@ import pandas as pd
 import cv2
 from time import time
 
+
 #Creating paths for saving cropped images.
 if not os.path.exists('./cropped_images/'):
     os.makedirs('./cropped_images/')
 
-#Defining path to the data
-annotation_file = './data/Anno/'
-identity_file = annotation_file + 'identity_CelebA.txt'
-bbox_file = annotation_file + 'list_bbox_celeba.txt'
 
-#Loading the image names (image) and labels (image_id)
-identity = pd.read_csv(identity_file, sep = " ", header = None,
+#Function for reading the images' names, labels and bounding boxes
+def reading_crop_inputs(identity_file = './data/Anno/identity_CelebA.txt', bbox_file = './data/Anno/list_bbox_celeba.txt'):
+    #Loading the image names (image) and labels (image_id)
+    identity = pd.read_csv(identity_file, sep = " ", header = None,
                         names = ['image', 'image_id'])
+    #Loading the bounding boxes of images
+    bbox = pd.read_csv(bbox_file, delim_whitespace = True)
 
-#Loading the bounding boxes of images
-bbox = pd.read_csv(bbox_file, delim_whitespace = True)
+    return identity, bbox
 
-#Function for cropping and resizing pictures with further exporting into .jpg format.
-def face_crop_export(id_df, bbox_df, crop_all = False):
+identity, bbox = reading_crop_inputs()
+
+
+#Function for cropping, resizing and exporting a single image in .jpg format.
+def face_crop(image_name, bbox_df):
+
+        #Loading Image
+        image_path = './data/Img/img_celeba/' + image_name
+        img = cv2.imread(image_path)
+
+        #Setting bounding box coordinates
+        startX = bbox_df[bbox_df['image_id'] == image_name]['x_1'].values[0]
+        startY = bbox_df[bbox_df['image_id'] == image_name]['y_1'].values[0]
+        endX = startX + bbox_df[bbox_df['image_id'] == image_name]['width'].values[0]
+        endY = startY + bbox_df[bbox_df['image_id'] == image_name]['height'].values[0]
+    
+        #Cropping
+        crop_img = img[startY:endY, startX:endX]
+        output_img = crop_img
+            
+        #Resizing
+        output_img = cv2.resize(crop_img, (224, 224))
+
+        #Exporting the cropped image
+        cv2.imwrite(f'./cropped_images/{image_name}', output_img)
+
+
+#Function for cropping all the images
+def face_crop_export(id_df, crop_all = False):
 
     #Start time initialization (for execution time measurement)
     start_time = time()
 
-    #Function for cropping image
-    def face_crop(image_name, bbox_df):
-
-            #Loading Image
-            image_path = './data/Img/img_celeba/' + image_name
-            img = cv2.imread(image_path)
-
-            #Setting bounding box coordinates
-            startX = bbox_df[bbox_df['image_id'] == image_name]['x_1'].values[0]
-            startY = bbox_df[bbox_df['image_id'] == image_name]['y_1'].values[0]
-            endX = startX + bbox_df[bbox_df['image_id'] == image_name]['width'].values[0]
-            endY = startY + bbox_df[bbox_df['image_id'] == image_name]['height'].values[0]
-    
-            #Cropping
-            crop_img = img[startY:endY, startX:endX]
-            output_img = crop_img
-            
-            #Resizing
-            output_img = cv2.resize(crop_img, (224, 224))
-
-            #Exporting the cropped image
-            cv2.imwrite(f'./cropped_images/{image_name}', output_img)
-
     #List of images to crop (whether to crop all the images in the identity file or only those images which haven't been cropped yet)
     imgs_to_crop = id_df['image'] if crop_all == True else list(set(id_df['image'].tolist()).difference(os.listdir("./cropped_images/")))
 
-    #Initialization for counting number of pictures which cannot be cropped.
+    #Initialization for counting number of images which cannot be cropped.
     k = 0
 
     #Crop each image
@@ -64,13 +68,13 @@ def face_crop_export(id_df, bbox_df, crop_all = False):
             face_crop(img, bbox)
 
             #Print statement
-            no_imgs = f'{ind + 1 - k}/{len(imgs_to_crop)}' #How many pictures have been cropped so far
-            speed = (ind + 1- k)/(time() - start_time) * 60 #How many pictures have been cropped per minute on average
+            no_imgs = f'{ind + 1 - k}/{len(imgs_to_crop)}' #How many images have been cropped so far
+            speed = (ind + 1- k)/(time() - start_time) * 60 #How many images have been cropped per minute on average
             eta = (len(imgs_to_crop) - (ind + 1 - k)) / ((ind + 1 - k)/(time() - start_time)) / 60 #Estimated remaining execution time
         
             print(f"{no_imgs} images cropped ... {speed:.2f} images/min | ETA: {eta:.2f} minutes", end = '\r')
 
-        #Skip if the picture has no applicable bounding boxes.    
+        #Skip if the image has no applicable bounding boxes.    
         except cv2.error as e:
             print(f"Image {img} has no applicable bounding boxes.")
             k += 1
